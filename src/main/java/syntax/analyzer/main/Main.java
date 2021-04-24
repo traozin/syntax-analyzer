ef7af1108f1775aa5cac93ee23146755d47c6d76
Antônio Neto <acm.neto1999@gmail.com>
2021-04-24 16:00:53 -0300

    First commit

---
+++ src/main/java/syntax/analyzer/main/Main.java
@@ -0,0 +1,13 @@
+package syntax.analyzer.main;
+
+/**
+ *
+ * @author Antonio Neto e Uellington Damasceno
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
